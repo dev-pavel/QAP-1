80be0e169be353b565ada570c4d2bed2d5d982d0
Pavel Pliuiko <pavel.pliuiko@keyin.com>
2024-10-07 21:25:28 -0230

    added tests for Bank class

---
+++ src/test/java/com/keyin/BankTest.java
@@ -0,0 +1,47 @@
+package com.keyin;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankTest {
+
+    @Test
+    void testAddAccount() {
+        Bank bank = new Bank();
+        BankAccount account = new BankAccount("12345", "Savings");
+        bank.addAccount(account);
+        assertEquals(1, bank.getAccounts().size());
+    }
+
+    @Test
+    void testTransferFunds() {
+        Bank bank = new Bank();
+        BankAccount account1 = new BankAccount("12345", "Checking");
+        BankAccount account2 = new BankAccount("67890", "Savings");
+
+        account1.deposit(200.0);
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+
+        boolean success = bank.transferFunds("12345", "67890", 100.0);
+        assertTrue(success);
+        assertEquals(100.0, account1.getBalance());
+        assertEquals(100.0, account2.getBalance());
+    }
+
+    @Test
+    void testTransferInsufficientFunds() {
+        Bank bank = new Bank();
+        BankAccount account1 = new BankAccount("12345", "Checking");
+        BankAccount account2 = new BankAccount("67890", "Savings");
+
+        account1.deposit(50.0);
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+
+        boolean success = bank.transferFunds("12345", "67890", 100.0);
+        assertFalse(success);
+        assertEquals(50.0, account1.getBalance());
+        assertEquals(0.0, account2.getBalance());
+    }
+}
