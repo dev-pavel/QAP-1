029a788a08d79bb70e57029f46cd6ca9b59598db
Pavel Pliuiko <pavel.pliuiko@keyin.com>
2024-10-07 21:26:51 -0230

    added test for Transaction class

---
+++ src/test/java/com/keyin/TransactionTest.java
@@ -0,0 +1,26 @@
+package com.keyin;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionTest {
+
+    @Test
+    void testTransactionCreation() {
+        Transaction transaction = new Transaction("Deposit", 100.0);
+        assertEquals("Deposit", transaction.getType());
+        assertEquals(100.0, transaction.getAmount());
+        assertNotNull(transaction.getTimestamp());
+    }
+
+    @Test
+    void testTransactionHistory() {
+        BankAccount account = new BankAccount("12345", "Checking");
+        account.deposit(100.0);
+        account.withdraw(50.0);
+
+        assertEquals(2, account.getTransactions().size());
+        assertEquals("Deposit", account.getTransactions().get(0).getType());
+        assertEquals("Withdrawal", account.getTransactions().get(1).getType());
+    }
+}
