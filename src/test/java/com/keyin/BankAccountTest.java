e659bd23594c3cdbb601187aab4836734dcb59a9
Pavel Pliuiko <pavel.pliuiko@keyin.com>
2024-10-07 21:27:41 -0230

    added test for BankAccount class

---
+++ src/test/java/com/keyin/BankAccountTest.java
@@ -0,0 +1,30 @@
+package com.keyin;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @Test
+    void testDeposit() {
+        BankAccount account = new BankAccount("12345", "Checking");
+        account.deposit(100.0);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    void testWithdraw() {
+        BankAccount account = new BankAccount("12345", "Savings");
+        account.deposit(200.0);
+        boolean success = account.withdraw(100.0);
+        assertTrue(success);
+        assertEquals(100.0, account.getBalance());
+    }
+
+    @Test
+    void testInsufficientFunds() {
+        BankAccount account = new BankAccount("12345", "Checking");
+        boolean success = account.withdraw(100.0);
+        assertFalse(success);
+    }
+}
